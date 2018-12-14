7b7a17ed5f80968801b1a376e7a8eb5f4874f3e9
ikeogu <ikeogu322@gmail.com>
2018-12-14 13:19:16 +0100

    tired

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-  <link href="{{asset('css/material-kit.css')}}" rel="stylesheet" />
+  <link href="css/material-kit.css" rel="stylesheet" />
   
 </head>
 <body class="index-page sidebar-collapse">  
