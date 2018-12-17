468935a6b30673e109116728267ccec3bfe0af5e
ikeogu <ikeogu322@gmail.com>
2018-12-17 16:32:16 +0100

    djkfkdj

--- resources/views/mgbidi-2019/index.blade.php
+++ resources/views/mgbidi-2019/index.blade.php
@@ -2,34 +2,18 @@
 
 @section('content')
 
-   <section class="feature-area pt-100 pb-100  relative" style="padding-top: 60px;">
-  <center> <h3>All Registered Choristers For Mgbidi 2019 Camp</h3> </center>
-  <div class="white-bg">
-		    <div class="container">
-		      <div class="section-top-border">
 
-
-		       
-              <div class="row">
-                 
-                  <div class="content">
-                    <div class="container-fluid">
-                      <div class="row">
-                        <div class="col-md-12">
-                          <div class="card">
-                            <div class="card-header card-header-primary">
-                              <h4 class="card-title ">Reg Details</h4>
-                              <p class="card-category"> All registrations so far</p>
-                            </div>
-                            <div class="card-body">
-                              <div class="table-responsive">
-                                <table class="table">
-                                  <thead class=" text-primary">
-                                    <tr><th>
-                                      ID
-                                    </th>
-                                    
-                                    <th>
+<div class="col-md-12" style="padding-top:60px;">
+              <div class="card card-plain">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title mt-0"> All Registered Choristers For Mgbidi 2019 Camp</h4>
+                  <p class="card-category">From Various state</p>
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table table-hover">
+                      <thead class="">
+                      <th>
                                       Name
                                     </th>
                                     <th>
@@ -47,13 +31,11 @@
                                     <th>
                                       Action
                                     </th>
-                                  </tr></thead>
-                                  @foreach($users as $user)
-                                  <tbody>
-                                    <tr>
-                                      <td>
-                                         {{$user->id}}
-                                      </td>
+                      </thead>
+                      <tbody>
+                        @foreach($users as $user)
+                        <tr>
+                                      
                                       
                                       <td>
                                           {{$user->firstname .' '. $user->lastname}}
@@ -75,25 +57,15 @@
                                    
                                       <td><a href="{{action('MgbidiController@downloadPDF', $user->id)}}"> Download PDF</a></td>
                                       
-                                    </tr>
-                                  </tbody>
-                                  @endforeach
-                                </table>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        
+                                    </tr>   
+                       @endforeach 
+                      </tbody>
+                    </table>
                   </div>
-                                
-                                
-                       
-                 
-               
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>  
-</section>
-         
-@endsection
+      </div>
+     @endsection 
