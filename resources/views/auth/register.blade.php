228496e411dc42435a5eede253d8dc02f45120c6
ikeogu <ikeogu322@gmail.com>
2018-12-17 07:29:36 +0100

    pic

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,24 +7,24 @@
       <div class="row">
         <div class="col-lg-4 col-md-6 ml-auto mr-auto" style="padding-top:123px; padding-bottom:100px;">
           <div class="card card-login" >
-          <div class="content">
-                            @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div><br />
-                            @endif
-                            @if (\Session::has('success'))
-                            <div class="alert alert-success">
-                                <p>{{ \Session::get('success') }}</p>
-                            </div><br />
-                            @endif
-                        </div>
-          <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-                        @csrf
+            <div class="content">
+              @if ($errors->any())
+              <div class="alert alert-danger">
+                  <ul>
+                      @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                      @endforeach
+                  </ul>
+              </div><br />
+              @endif
+              @if (\Session::has('success'))
+              <div class="alert alert-success">
+                  <p>{{ \Session::get('success') }}</p>
+              </div><br />
+              @endif
+            </div>
+            <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+                          @csrf
               <div class="card-header card-header-primary text-center">
                 <h4 class="card-title">Be A Member</h4>
                 
@@ -39,10 +39,10 @@
                   </div>
                   <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus placeholder="Full Name...">
                   @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $errors->first('name') }}</strong>
+                      </span>
+                  @endif
                 </div>
                 <div class="input-group">
                   <div class="input-group-prepend">
@@ -66,11 +66,11 @@
                     </span>
                   </div>
                     <input type="text" class="form-control{{ $errors->has('state') ? ' is-invalid' : '' }}" name="state" value="{{ old('state') }}" required autofocus placeholder="State...">
-                                @if ($errors->has('state'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('state') }}</strong>
-                                    </span>
-                                @endif
+                      @if ($errors->has('state'))
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $errors->first('state') }}</strong>
+                          </span>
+                      @endif
                   </div>              
                 </div>
                 <div class="input-group">
@@ -81,10 +81,10 @@
                   </div>
                   <input type="text" class="form-control{{ $errors->has('discipline') ? ' is-invalid' : '' }}" name="discipline" value="{{ old('discipline') }}" required autofocus placeholder="Discipline...">
                   @if ($errors->has('discipline'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('discipline') }}</strong>
-                                    </span>
-                                @endif
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $errors->first('discipline') }}</strong>
+                      </span>
+                  @endif
                 </div>
 
                 <div class="input-group">
@@ -95,10 +95,10 @@
                   </div>
                   <input type="text" class="form-control{{ $errors->has('institution') ? ' is-invalid' : '' }}" name="institution" value="{{ old('institution') }}" required autofocus placeholder="Institution...">
                   @if ($errors->has('institution'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('institution') }}</strong>
-                                    </span>
-                                @endif
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $errors->first('institution') }}</strong>
+                      </span>
+                  @endif
                 </div>
                 <div class="input-group">
                   <div class="input-group-prepend">
@@ -113,19 +113,19 @@
                  </select>
 
                   @if ($errors->has('gender'))
-                              <span class="invalid-feedback" role="alert">
-                                  <strong>{{ $errors->first('gender') }}</strong>
-                              </span>
-                          @endif
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $errors->first('gender') }}</strong>
+                      </span>
+                  @endif
 
-                   <div class="input-group-prepend">
+                  <div class="input-group-prepend">
                     
                   </div>
                   <input type="text" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required autofocus placeholder="Phone...">
                       @if ($errors->has('phone'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('phone') }}</strong>
-                                        </span>
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('phone') }}</strong>
+                        </span>
                       @endif       
                 </div> 
                 
@@ -137,11 +137,11 @@
                       </div>
                         <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeHolder=" Password">
 
-                                    @if ($errors->has('password'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                    @endif
+                          @if ($errors->has('password'))
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $errors->first('password') }}</strong>
+                              </span>
+                          @endif
                       
                     </div>  
                     <div class="input-group">
@@ -150,13 +150,10 @@
                           <i class="material-icons">key</i>
                         </span>
                       </div>
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required placeHolder="Confirm Password">
+                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required placeHolder="Confirm Password">
                     </div>
                 </div>
                
-                
-                       
-             </div>
               <div class="footer text-center">
               <button class="btn btn-primary btn-round">Register</button>
               </div>
