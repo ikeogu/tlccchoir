019e70ccc2bf80a86beedc3f783c34d3af92440a
ikeogu <ikeogu322@gmail.com>
2018-12-18 09:42:48 +0100

    added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -42,7 +42,7 @@ class AdminController extends Controller
 
     public function delup(){
       
-        return view('admin/delectuploadedimages');
+        return view('admin/deleteuploadedimages');
        
 
     }
