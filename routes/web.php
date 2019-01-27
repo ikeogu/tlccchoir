68e3baf2d63a1a64398ef71bf24fecb914c09004
ikeogu <ikeogu322@gmail.com>
2019-01-27 04:06:38 +0100

    erorrs

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::group(['middleware' => ['web']], function(){
 Route::get('/', function () {
     return View::make('index');
 });
-
+Auth::routes();
 Route::get('/home','HomeController@index' );
 
 
