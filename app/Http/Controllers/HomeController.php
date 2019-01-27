b45896b134845a9c54f6cac54bce44d92320f296
ikeogu <ikeogu322@gmail.com>
2019-01-27 21:26:37 +0100

    kdfgk

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
     
     public function index()
     {
-        return view('index');
+        return view('users/index');
 
     }
    
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,12 @@
 */
 Route::group(['middleware' => ['web']], function(){
 
-Route::get('/', function () {
-    return view('index');
-});
-Auth::routes();
-Route::get('/home','HomeController@index' );
+	Route::get('/', function () {
+		return view('index');
+	})->name('homepage');
+	Auth::routes();
+	Route::get('/home','HomeController@index');
+	
 
 
 
@@ -28,6 +29,7 @@ Route::get('/write_song', 'SongPresentationCOntroller@create')->name('create');
 
 Route::get('/about_us', 'AboutCOntroller@index')->name('about');
 //for User Controller
+
 
 
 Route::get('mysongs', 'SongPresentationCOntroller@mysongs')->name('withsonginprofile');
