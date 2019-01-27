8e40684e4ccd1c1391822f32a6a7bafe92371da3
ikeogu <ikeogu322@gmail.com>
2019-01-28 00:18:00 +0100

    working on blog

---
+++ database/migrations/2019_01_27_231354_create_posts_u_pdate_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsUPdateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            
+            $table->string('type')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts_u_pdate');
+    }
+}
