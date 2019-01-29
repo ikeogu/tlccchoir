097e4c209d38758f4254008908f3e62a0642e830
ikeogu <ikeogu322@gmail.com>
2019-01-29 07:19:01 +0100

    fixing display

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,6 +13,7 @@ use App\NorthernStates;
 use App\EasthernStates;
 use App\WesthernStates;
 use App\SouthernStates;
+use DB;
 
 class AdminController extends Controller
 {
@@ -99,157 +100,157 @@ class AdminController extends Controller
     }
     // for south east
     public function abia(){
-        $user= User::where('state','=','abia')->get();
+        $user= User::where('state','=','Abia')->get();
         return view('admin/abia',['auser'=>$user]);
     }
     public function anambra(){
-        $user= User::where('state','=','anambra')->get();
+        $user= User::where('state','=','Anambra')->get();
         return view('admin/anambra',['auser'=>$user]);
     }
     public function ebonyi(){
-        $user= User::where('state','=','ebonyi')->get();
+        $user= User::where('state','=','Ebonyi')->get();
         return view('admin/ebonyi',['auser'=>$user]);
     }
     public function enugu(){
-        $user= User::where('state','=','enugu')->get();
+        $user= User::where('state','=','Enugu')->get();
         return view('admin/enugu',['auser'=>$user]);
     }
     public function imo(){
-        $user= User::where('state','=','imo')->get();
+        $user= User::where('state','=','Imo')->get();
         return view('admin/imo',['auser'=>$user]);
     }
 
     // for south south
     public function akwaibom(){
-        $user= User::where('state', '=','akwa-ibom')->get();
+        $user= User::where('state', '=','Akwa-ibom')->get();
         return view('admin/akwa-ibom',['auser'=>$user]);
     }
     public function bayelsa(){
-        $user= User::where('state','=','bayelsa')->get();
+        $user= User::where('state','=','Bayelsa')->get();
         return view('admin/bayelsa',['auser'=>$user]);
     }
     public function crossriver(){
-        $user= User::where('state', '=','cross-river')->get();
+        $user= User::where('state', '=','Cross-River')->get();
         return view('admin/cross-river',['auser'=>$user]);
     }
     public function delta(){
-        $user= User::where('state', '=','delta')->get();
+        $user= User::where('state', '=','Delta')->get();
         return view('admin/delta',['auser'=>$user]);
     }
     public function edo(){
-        $user= User::where('state','=','edo')->get();
+        $user= User::where('state','=','Edo')->get();
         return view('admin/edo',['auser'=>$user]);
     }
     public function rivers(){
-        $user= User::where('state','=','rivers')->get();
+        $user= User::where('state','=','Rivers')->get();
         return view('admin/rivers',['auser'=>$user]);
     }
     // for south west
     public function lagos(){
-        $user= User::where('state','=','lagos')->get();
+        $user= User::where('state','=','Lagos')->get();
         return view('admin/lagos',['auser'=>$user]);
     }
     public function ekiti(){
-        $user= User::where('state','=','ekiti')->get();
+        $user= User::where('state','=','Ekiti')->get();
         return view('admin/ekiti',['auser'=>$user]);
     }
     public function ogun(){
-        $user= User::where('state','=','ogun')->get();
+        $user= User::where('state','=','Ogun')->get();
         return view('admin/ogun',['auser'=>$user]);
     }
     public function ondo(){
-        $user= User::where('state','=','ondo')->get();
+        $user= User::where('state','=','Ondo')->get();
         return view('admin/ondo',['auser'=>$user]);
     }
     public function osun(){
-        $user= User::where('state','=','osun')->get();
+        $user= User::where('state','=','Osun')->get();
         return view('admin/osun',['auser'=>$user]);
     }
     public function oyo(){
-        $user= User::where('state','=','oyo')->get();
+        $user= User::where('state','=','Oyo')->get();
         return view('admin/oyo',['auser'=>$user]);
     }
     
     //for north state
     public function benue(){
-        $user= User::where('state','=','benue')->get();
+        $user= User::where('state','=','Benue')->get();
         return view('admin/benue',['auser'=>$user]);
     }
     public function kogi(){
-        $user= User::where('state','=','kogi')->get();
+        $user= User::where('state','=','Kogi')->get();
         return view('admin/kogi',['auser'=>$user]);
     }
     public function kwara(){
-        $user= User::where('state','kwara')->get();
+        $user= User::where('state','=','Kwara')->get();
         return view('admin/kwara',['auser'=>$user]);
     }
     public function nassarawa(){
-        $user= User::where('state','nassarawa')->get();
+        $user= User::where('state','=','Nassarawa')->get();
         return view('admin/nassarawa',['auser'=>$user]);
     }
     public function niger(){
-        $user= User::where('state','=','niger')->get();
+        $user= User::where('state','=','Niger')->get();
         return view('admin/niger',['auser'=>$user]);
     }
     public function plateau(){
-        $user= User::where('state','=','plateau')->get();
+        $user= User::where('state','=','Plateau')->get();
         return view('admin/plateau',['auser'=>$user]);
     }
     public function abuja(){
-        $user= User::where('state','=','abuja')->get();
+        $user= User::where('state','=','Abuja')->get();
         return view('admin/abuja',['auser'=>$user]);
     }
     public function adamawa(){
-        $user= User::where('state','=','adamawa')->get();
+        $user= User::where('state','=','Adamawa')->get();
         return view('admin/adamawa',['auser'=>$user]);
     }
     public function bauchi(){
-        $user= User::where('state','=','bauchi')->get();
+        $user= User::where('state','=','Bauchi')->get();
         return view('admin/bauchi',['auser'=>$user]);
     }
     public function borno(){
-        $user= User::where('state','=','borno')->get();
+        $user= User::where('state','=','Borno')->get();
         return view('admin/borno',['auser'=>$user]);
     }
     public function gombe(){
-        $user= User::where('state','=','gombe')->get();
+        $user= User::where('state','=','Gombe')->get();
         return view('admin/gombe',['auser'=>$user]);
     }
     public function taraba(){
-        $user= User::where('state','=','taraba')->get();
+        $user= User::where('state','=','Taraba')->get();
         return view('admin/taraba',['auser'=>$user]);
     }
 
     public function yobe(){
-        $user= User::where('state','=','yobe')->get();
+        $user= User::where('state','=','Yobe')->get();
         return view('admin/yobe',['auser'=>$user]);
     }
     public function jigawa(){
-        $user= User::where('state','=','jigawa')->get();
+        $user= User::where('state','=','Jigawa')->get();
         return view('admin/jigawa',['auser'=>$user]);
     }
     public function kaduna(){
-        $user= User::where('state','=','kaduna')->get();
+        $user= User::where('state','=','Kaduna')->get();
         return view('admin/kaduna',['auser'=>$user]);
     }
     public function kano(){
-        $user= User::where('state','=','kano')->get();
+        $user= User::where('state','=','Kano')->get();
         return view('admin/kano',['auser'=>$user]);
     }
     public function kastina(){
-        $user= User::where('state','=','kastina')->get();
+        $user= User::where('state','=','Kastina')->get();
         return view('admin/kastina',['auser'=>$user]);
     }
     public function kebbi(){
-        $user= User::where('state','=','kebbi')->get();
+        $user= User::where('state','=','Kebbi')->get();
         return view('admin/kebbi',['auser'=>$user]);
     }
     public function sokoto(){
-        $user= User::where('state','=','sokoto')->get();
+        $user= User::where('state','=','Sokoto')->get();
         return view('admin/sokoto',['auser'=>$user]);
     }
     public function zamfara(){
-        $user= User::where('state','=','zamfara')->get();
+        $user= User::where('state','=','Zamfara')->get();
         return view('admin/zamfara',['auser'=>$user]);
     }
 
