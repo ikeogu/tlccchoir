c5d5d4403a6a242379f63aa0a2fa41413936784f
ikeogu <ikeogu322@gmail.com>
2019-01-31 17:34:09 +0100

    kl

--- database/migrations/2019_01_31_162807_drop_comment_table.php
+++ database/migrations/2019_01_31_162807_drop_comment_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddImageColToPost extends Migration
+class DropCommentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,7 @@ class AddImageColToPost extends Migration
      */
     public function up()
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->string('image')->nullable()->after('slug');
-        });
+        Schema::drop('posts');
     }
 
     /**
@@ -25,8 +23,6 @@ class AddImageColToPost extends Migration
      */
     public function down()
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->dropColumn('image');
-        });
+        //
     }
 }
--- database/migrations/2019_01_31_162904_drop_post_table.php
+++ database/migrations/2019_01_31_162904_drop_post_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCommentsTable extends Migration
+class DropPostTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::drop('comments');
+       Schema::drop('comments');
     }
 
     /**
@@ -23,7 +23,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        
-        Schema::dropIfExists('comments');
+        //
     }
 }
