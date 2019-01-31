471b531be247088b766bd1d70483a5c2339e8efc
ikeogu <ikeogu322@gmail.com>
2019-01-31 17:04:29 +0100

    site 70 percent

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -126,6 +126,24 @@
           </div>
           </div>
           <div class="main-panel">
+          <!-- Navbar -->
+          <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top " id="navigation-example">
+              <div class="container-fluid">
+              <div class="navbar-wrapper">
+                  
+              </div>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation" data-target="#navigation-example">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="navbar-toggler-icon icon-bar"></span>
+                  <span class="navbar-toggler-icon icon-bar"></span>
+                  <span class="navbar-toggler-icon icon-bar"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-end">
+                  
+                 
+              </div>
+              </div>
+          </nav>
          
           <main class="py-4">
               @yield('content')
