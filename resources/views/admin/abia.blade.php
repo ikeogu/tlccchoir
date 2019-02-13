c654d09e639effe589b91133cb6158e861fa0552
ikeogu <ikeogu322@gmail.com>
2019-02-13 18:23:06 +0100

    fixing logim

--- resources/views/admin/abia.blade.php
+++ resources/views/admin/abia.blade.php
@@ -3,8 +3,8 @@
 <div class="col-md-12" style="padding-top:60px;">
               <div class="card card-plain">
                 <div class="card-header card-header-primary">
-                  <h4 class="card-title mt-0"> All Registered Members of the Campus Choir</h4>
-                  <p class="card-category">From Various state</p>
+                  <h4 class="card-title mt-0"> Registered Members of the Campus Choir</h4>
+                  <p class="card-category">From Abia state</p>
                 </div>
                 <div class="card-body">
                   <div class="table-responsive">
